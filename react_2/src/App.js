5985d94576734ce803e5f04e231f5d69d7857ef9
Sainteric93hd <58420787+Sainteric93hd@users.noreply.github.com>
2021-08-11 16:47:07 +0700

    Lap 1
    Lap 1

---
+++ react_2/src/App.js
@@ -0,0 +1,11 @@
+import logo from './logo.svg';
+import './App.css';
+import countDownComponent from "./component/countDownComponent";
+function App() {
+  return (
+     countDownComponent()
+
+  );
+}
+
+export default App;
---
+++ react_2/src/component/countDownComponent.js
@@ -0,0 +1,29 @@
+
+
+export default function countDownComponent(){
+    function startTimer(duration, display) {
+        var timer = duration, minutes, seconds;
+        setInterval(function () {
+            minutes = parseInt(timer / 60, 10);
+            seconds = parseInt(timer % 60, 10);
+
+            minutes = minutes < 10 ? "0" + minutes : minutes;
+            seconds = seconds < 10 ? "0" + seconds : seconds;
+
+            display.textContent = minutes + ":" + seconds;
+
+            if (--timer < 0) {
+                timer = duration;
+            }
+        }, 1000);
+    }
+
+    window.onload = function () {
+        var fiveMinutes = 60 * 5,
+            display = document.querySelector('#time');
+        startTimer(fiveMinutes, display);
+    };
+    return (
+        <div> <span id="time">05:00</span> </div>
+    );
+}
